da7de911a30b456929f1e7aafa11aeb9287ca367
Nikolay <medvedev.sibers@gmail.com>
2023-09-10 16:21:47 +0700

    without display control, not tested

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,4 @@
-#include <lvgl.h>
 #include <TFT_eSPI.h>
-#include <ui.h>
 #include <Wire.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
@@ -27,25 +25,25 @@ extern "C" {
 #define radTempPin2 25
 #define waterTempPin 34
 
-TaskHandle_t TaskPel_t;
-TaskHandle_t TaskPel2_t;
-TaskHandle_t TaskFan_t;
+TaskHandle_t TaskTempCheck_t;
+TaskHandle_t TaskPelControl_t;
 TaskHandle_t TaskFan1_t;
 TaskHandle_t TaskFan2_t;
 TaskHandle_t TaskPump_t;
 TimerHandle_t TimerCooldown_t;
 TimerHandle_t TimerCooldown2_t;
 QueueHandle_t QueuePump;
+QueueHandle_t QueueScreenTemp;
+QueueHandle_t QueueCoolingCommands;
 
 Adafruit_SSD1306 display(128, 64, &Wire, -1);
 
 int power = 0;
 bool debug = true;
-bool pumpstate = false;
-bool pumpstate2 = false;
-bool pumpflag = false;
 int8_t correction = -2;
 byte pumpPower = 255;
+bool pelOverheat1 = false;
+bool pelOverheat2 = false;
 
 char errRadOverheat[] = "RADIATOR OVERHEATING, PELTIET POWER SUPPLY DISCONNECTED, PUMP STOPPED";
 char errRadTempSensor[] = "Radiator temp sensor malfunction, initializing procedure stopped, check connections and restart the system";
@@ -59,28 +57,12 @@ struct pumpData {
     bool reqState;
 };
 
+struct CoolingCommand {
+    byte pelNumber;
+    bool reqState;
+};
+
 pumpData pel;
-
-
-/*Don't forget to set Sketchbook location in File/Preferencesto the path of your UI project (the parent foder of this INO file)*/
-
-/*Change to your screen resolution*/
-static const uint16_t screenWidth  = 320;
-static const uint16_t screenHeight = 240;
-
-static lv_disp_draw_buf_t draw_buf;
-static lv_color_t buf[ screenWidth * screenHeight / 10 ];
-
-// TFT_eSPI tft = TFT_eSPI(screenHeight, screenWidth); /* TFT instance */
-
-// #if LV_USE_LOG != 0
-// /* Serial debugging */
-// void my_print(const char * buf)
-// {
-//     Serial.printf(buf);
-//     Serial.flush();
-// }
-// #endif
 
 float temp (byte tpin) {
     float tdata;
@@ -91,9 +73,6 @@ float temp (byte tpin) {
     double tempK = 1 / (1 / (273.15 + 25) + log(Rt / 10) / 3950.0);
     double tempCRaw = tempK - 273.15;
     double tempC = tempCRaw + correction;
-    // lv_bar_set_value(ui_tempbar, tempC, LV_ANIM_ON);
-    // lv_snprintf(temptext, sizeof(temptext), "%d",(int)tempC);
-    // lv_label_set_text(ui_tempval, temptext);
     return tempC;
 }
 
@@ -117,12 +96,20 @@ void errprint (char *err) {
 }
 
 void cooldownTimer () {
+    struct CoolingCommand command;
+    struct pumpData pump;
     int radTemp = temp(radTempPin);
     if (radTemp < 50) {
-        vTaskResume (TaskPel_t);
-        pel.pelNumber = 1;
-        pel.reqState = true;
+        pelOverheat1 = false;
+        pump.pelNumber = 1;
+        pump.reqState = true;
         xQueueSend(QueuePump, &pel, portMAX_DELAY);
+        int waterTemp = temp(waterTempPin);
+        if (waterTemp >= 20) {
+            command.pelNumber = 1;
+            command.reqState = true;
+            xQueueSend(QueueCoolingCommands, &command, 500);
+        }
         Serial.println (msgRadCooled);
         errprint (msgRadCooled);
         xTimerStop (TimerCooldown_t, 0);
@@ -142,12 +129,20 @@ void cooldownTimer () {
 }
 
 void cooldownTimer2 () {
+    struct CoolingCommand command;
+    struct pumpData pump;
     int radTemp = temp(radTempPin2);
     if (radTemp < 50) {
-        vTaskResume (TaskPel2_t);
-        pel.pelNumber = 2;
-        pel.reqState = true;
+        pelOverheat2 = false;
+        pump.pelNumber = 2;
+        pump.reqState = true;
         xQueueSend(QueuePump, &pel, portMAX_DELAY);
+        int waterTemp = temp(waterTempPin);
+        if (waterTemp >= 20) {
+            command.pelNumber = 2;
+            command.reqState = true;
+            xQueueSend(QueueCoolingCommands, &command, 500);
+        }
         Serial.println (msgRadCooled);
         errprint (msgRadCooled);
         xTimerStop (TimerCooldown2_t, 0);
@@ -166,46 +161,65 @@ void cooldownTimer2 () {
     }
 }
 
-// bool fancontrol (byte channel, int temp, byte pel) {
-//         if (temp < 30) {
-//             ledcWrite (channel, 0);
-//             if (debug == true) {
-//             Serial.println("FANCONTROL - POWER ZERO");
-//             }
-//             return true;
-//         }
-//         else if (temp >= 30 && temp <= 70 ) {
-//             int pow = map(temp, 30, 70, 60, 255);
-//             ledcWrite (channel, pow);
-//             if (debug == true) {
-//             Serial.println("FANCONTROL - POWER: ");
-//             Serial.println(pow);
-//             }
-//             return true;  
-//         }
-//         else if (temp > 70) {
-//             vTaskSuspend (TaskPel_t);
-//             digitalWrite(pel, LOW);
-//             ledcWrite (channel, 255);
-//             if (debug == true) {
-//                 Serial.println(errRadOverheat);
-//             }
-//             errprint(errRadOverheat);
-//             if (channel == 1) {
-//                 if (debug == true) {
-//                     Serial.println("FANCONTROL - 1 TIMER STARTED");
-//                 }
-//                 xTimerStart (TimerCooldown_t, 0);
-//             }
-//             else if (channel == 2) {
-//                 if (debug == true) {
-//                     Serial.println("FANCONTROL - 2 TIMER STARTED");
-//                 }
-//                 xTimerStart (TimerCooldown2_t, 0);
-//             }
-//             return false;
-//     }
-// }
+void TaskTempCheck (void *pvParameters) {
+    struct CoolingCommand command;
+    static int lastWaterTemp;
+    bool reqCoolingState;
+    for (;;) {
+    int currentWaterTemp = temp (waterTempPin);
+    if (lastWaterTemp != currentWaterTemp) {
+        lastWaterTemp = currentWaterTemp;
+        xQueueSend(QueueScreenTemp, &currentWaterTemp, portMAX_DELAY);
+    }
+    if (currentWaterTemp >= 20) {
+        if (pelOverheat1 == false) {
+            command.pelNumber = 1;
+            command.reqState = true;
+            xQueueSend(QueueCoolingCommands, &command, portMAX_DELAY);
+        }
+        if (pelOverheat2 == false) {
+            command.pelNumber = 2;
+            command.reqState = true;
+            xQueueSend(QueueCoolingCommands, &command, portMAX_DELAY);
+        }
+        if (debug == true) {
+            Serial.println("COOLING COMMAND SEND - true");
+        }
+    }
+    else if (currentWaterTemp <= 17) {
+        command.pelNumber = 1;
+        command.reqState = false;
+        xQueueSend(QueueCoolingCommands, &command, portMAX_DELAY);
+        command.pelNumber = 2;
+        command.reqState = false;
+        xQueueSend(QueueCoolingCommands, &command, portMAX_DELAY);
+        if (debug == true) {
+            Serial.println("COOLING COMMAND SEND - false");
+        }
+    }
+    vTaskDelay(1000);
+    }
+}
+
+void TaskPelControl (void *pvParameters) {
+    struct CoolingCommand receivedCommand;
+    for (;;) {
+    xQueueReceive(QueueCoolingCommands, &receivedCommand, portMAX_DELAY);
+    if (receivedCommand.reqState == true) {
+        digitalWrite(receivedCommand.pelNumber, HIGH);
+        if (debug == true) {
+            Serial.printf("PELCONTROL - ", receivedCommand.pelNumber, " pin enabled");
+        }
+    }
+    else if (receivedCommand.reqState == false) {
+        digitalWrite(receivedCommand.pelNumber, LOW);
+        if (debug == true) {
+            Serial.printf("PELCONTROL - ", receivedCommand.pelNumber, " pin disabled");
+        }
+    }
+    vTaskDelay(100);
+    }
+}
 
 void TaskPel (void *pvParameters) {
     for (;;) {
@@ -257,63 +271,9 @@ void TaskPel2 (void *pvParameters) {
     }
 }
 
-// void TaskFan (void *pvParameters) {
-//     for (;;) {
-//         int radTemp = temp (radTempPin);
-//         if (radTemp < 35) {
-//             ledcWrite (0, 0);
-//         }
-//         else if (radTemp >= 40 && radTemp < 45 ) {
-//             ledcWrite (0, 55);
-//         }
-//         else if (radTemp >= 45 && radTemp < 50 ) {
-//             ledcWrite (0, 105);
-//         }
-//         else if (radTemp >= 50 && radTemp < 60 ) {
-//             ledcWrite (0, 185);
-//         }
-//         else if (radTemp >= 60 && radTemp < 70 ) {
-//             ledcWrite (0, 215);
-//         }
-//         else if (radTemp >= 75 && radTemp < 80 ) {
-//             ledcWrite (0, 255);
-//         }
-//         else if (radTemp > 85) {
-//             vTaskSuspend (TaskPel_t);
-//             digitalWrite(PELPIN, LOW);
-//             digitalWrite(PELPIN2, LOW);
-//             ledcWrite (0, 255);
-//             ledcWrite(1, 0);
-//             Serial.print(errRadOverheat);
-//             errprint(errRadOverheat);
-//             xTimerStart (TimerCooldown_t, 0);
-//         }
-//         vTaskDelay(1000);
-//     }
-// }
-// void TaskFan (void *pvParameters) {
-//     for (;;) {
-//         int radTemp = temp (radTempPin);
-//         int radTemp2 = temp (radTempPin2);
-//         bool fanstat1 = fancontrol(1, radTemp, PELPIN);
-//         if (debug == true) {
-//             Serial.println("FANTASK - FIRST RAD STATE:");
-//             Serial.print(fanstat1);
-//         }
-//         bool fanstat2 = fancontrol(2, radTemp2, PELPIN2);
-//         if (debug == true) {
-//             Serial.println("FANTASK - SECOND RAD STATE:");
-//             Serial.print(fanstat2);
-//         }
-//         if (fanstat1 == false && fanstat2 == false) {
-//             pumpstate = false;
-//             Serial.println ("Pump stopped");
-//         }
-//         vTaskDelay(1000);
-//     }
-// }
-
 void TaskFan1 (void *pvParameters) {
+    struct pumpData pump;
+    struct CoolingCommand command;
     for (;;) {
         int rtemp = temp (radTempPin);
         if (rtemp < 30) {
@@ -331,15 +291,17 @@ void TaskFan1 (void *pvParameters) {
             }
         }
         else if (rtemp > 70) {
-            vTaskSuspend (TaskPel_t);
-            digitalWrite(PELPIN, LOW);
+            command.pelNumber = PELPIN;
+            command.reqState = false;
+            pelOverheat1 = true;
+            xQueueSend(QueueCoolingCommands, &command, portMAX_DELAY);
             ledcWrite (0, 255);
             if (debug == true) {
                 Serial.println(errRadOverheat);
             }
             errprint(errRadOverheat);
-            pel.pelNumber = 1;
-            pel.reqState = false;
+            pump.pelNumber = 1;
+            pump.reqState = false;
             xQueueSend(QueuePump, &pel, portMAX_DELAY);
             xTimerStart (TimerCooldown_t, 0);
     }
@@ -348,6 +310,8 @@ void TaskFan1 (void *pvParameters) {
 }
 
 void TaskFan2 (void *pvParameters) {
+    struct pumpData pump;
+    struct CoolingCommand command;
     for (;;) {
         int rtemp = temp (radTempPin2);
         if (rtemp < 30) {
@@ -365,9 +329,11 @@ void TaskFan2 (void *pvParameters) {
             }
         }
         else if (rtemp > 70) {
-            vTaskSuspend (TaskPel2_t);
-            digitalWrite(PELPIN2, LOW);
-            ledcWrite (0, 255);
+            command.pelNumber = PELPIN2;
+            command.reqState = false;
+            pelOverheat2 = true;
+            xQueueSend(QueueCoolingCommands, &command, portMAX_DELAY);
+            ledcWrite (2, 255);
             if (debug == true) {
                 Serial.println(errRadOverheat);
             }
@@ -409,137 +375,6 @@ void TaskPump (void *pvParameters) {
     }
 }
 
-// void TaskPump (void *pvParameters) {
-//     struct pumpData received; -- старая функция помпы
-//     static bool pel1ReqState;
-//     static bool pel2ReqState;
-//     for (;;) { 
-//         if (pumpstate == false && pumpflag == true) {
-//             digitalWrite (PUMPPIN, LOW);
-//             pumpflag = false;
-//             if (debug == true) {
-//             Serial.println("pump stopped");
-//             }
-//         }
-//         else if (pumpstate == false && pumpflag == false) {
-//             if (debug == true) {
-//                 Serial.println ("pump already stopped");
-//             }
-//         }
-//         else if (pumpstate == true && pumpflag == false) {
-//             digitalWrite(PUMPPIN, HIGH);
-//             pumpflag = true;
-//             if (debug == true) {
-//             Serial.println("pump started");
-//             }
-//         }
-//         else if (pumpstate == true && pumpflag == true); {
-//             if (debug == true) {
-//             Serial.println("pump already started");
-//             }
-//         }
-//         vTaskDelay(1000);
-//     }
-// }
-// void init () {
-//     byte iterations = 30;
-//     int wtemps [iterations];
-//     int rtemps [iterations];
-//     for (int i = 0; i < iterations; i++) {
-//         wtemps[i] = temp(waterTempPin);
-//         delay(5);
-//     }
-//     qsort (wtemps, iterations, sizeof(int), sort_desc);
-//     int midwtemp = wtemps[iterations / 2];
-
-//     for (int i = 0; i < iterations; i++) {
-//         rtemps[i] = temp(radTempPin);
-//         delay(5);
-//     }
-//     qsort (rtemps, iterations, sizeof(int), sort_desc);
-//     Serial.println ("Temps");
-//     int midrtemp = rtemps[iterations / 2];
-//     Serial.println (midwtemp);
-//     Serial.println (midrtemp);
-//     if (midwtemp < -20 || midwtemp > 60) {
-//         Serial.println("Water temp sensor malfunction, initializing procedure stopped");
-//         Serial.println("Check connections and restart the system");
-//         oled.clear();
-//         oled.home();
-//         oled.autoPrintln("Water temp sensor malfunction, initializing procedure stopped. Check connections and restart the system");
-//         oled.update();
-//     }
-//     else if (midrtemp < -20 || midrtemp > 100) {
-//         Serial.println("Radiator temp sensor malfunction, initializing procedure stopped");
-//         Serial.println("Check connections and restart the system");
-//         // oled.clear();
-//         // oled.home();
-//         // oled.autoPrintln("Radiator temp sensor malfunction, initializing procedure stopped. Check connections and restart the system");
-//         // oled.update();
-//     }
-//     else {
-//         Serial.println("Temp sensors initialized, starting...");
-//         // oled.clear();
-//         // oled.home();
-//         // oled.autoPrintln("Temp sensors initialized, starting...");
-//         // oled.update();
-//         ledcWrite (1, pumpPower);
-//         xTaskCreatePinnedToCore(TaskFan, "TaskFan", 2048, NULL, 1, &TaskFan_t, 0);
-//         xTaskCreatePinnedToCore(TaskPel, "TaskPel", 2048, NULL, 1, &TaskPel_t, 0);
-//         TimerCooldown_t = xTimerCreate("TimerCooldown", pdMS_TO_TICKS(20000), pdTRUE, (void*)0, reinterpret_cast<TimerCallbackFunction_t>(cooldownTimer));
-//     }
-// }
-/* Display flushing */
-// void my_disp_flush( lv_disp_drv_t *disp, const lv_area_t *area, lv_color_t *color_p )
-// {
-//     uint32_t w = ( area->x2 - area->x1 + 1 );
-//     uint32_t h = ( area->y2 - area->y1 + 1 );
-
-//     tft.startWrite();
-//     tft.setAddrWindow( area->x1, area->y1, w, h );
-//     tft.pushColors( ( uint16_t * )&color_p->full, w * h, true );
-//     tft.endWrite();
-
-//     lv_disp_flush_ready( disp );
-// }
-
-/*Read the touchpad*/
-// void my_touchpad_read( lv_indev_drv_t * indev_driver, lv_indev_data_t * data )
-// {
-//     uint16_t touchX = 0, touchY = 0;
-
-//     bool touched = tft.getTouch( &touchX, &touchY, 600 );
-
-//     if( !touched )
-//     {
-//         data->state = LV_INDEV_STATE_REL;
-//     }
-//     else
-//     {
-//         data->state = LV_INDEV_STATE_PR;
-
-//         /*Set the coordinates*/
-//         data->point.x = touchX;
-//         data->point.y = touchY;
-
-//         // Serial.print( "Data x " );
-//         // Serial.println( touchX );
-
-//         // Serial.print( "Data y " );
-//         // Serial.println( touchY );
-//     }
-// }
-// void event_cb(lv_event_t * event) {
-//     char powtext[12];
-//     lv_snprintf(powtext, sizeof(powtext), "%d",(int)lv_slider_get_value(ui_power));
-//     lv_label_set_text(ui_VALUE, powtext);
-//     power = lv_slider_get_value(ui_power);
-//      ledcWrite(1, power);
-//     Serial.println(power);
-//     // Serial.println("temp");
-//     // Serial.println(temp());
-// }
-
 void setup()
 {
     Serial.begin( 115200 );
@@ -554,10 +389,11 @@ void setup()
     ledcSetup(2, PWMFREQ, 8); 
     ledcAttachPin(FANPIN, 0);
     ledcAttachPin(FANPIN2, 2);
-    // ledcAttachPin(PUMPPIN, 1);
     uint16_t calData[5] = { 285, 3645, 291, 3506, 1 };
-    QueuePump = xQueueCreate( 30  , sizeof( struct pumpData) );
-    // tft.setTouch(calData);
+    QueuePump = xQueueCreate(50  , sizeof( struct pumpData) );
+    QueueScreenTemp = xQueueCreate(50, sizeof(int));
+    QueueCoolingCommands = xQueueCreate(100, sizeof(struct CoolingCommand));
+
     display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
     display.clearDisplay();
     delay(2000);
@@ -612,53 +448,16 @@ void setup()
         pel.pelNumber = 2;
         pel.reqState = true;
         xQueueSend(QueuePump, &pel, portMAX_DELAY);
-        xTaskCreatePinnedToCore(TaskFan1, "TaskFan", 2048, NULL, 1, &TaskFan_t, 0);
-        xTaskCreatePinnedToCore(TaskFan2, "TaskFan", 2048, NULL, 1, &TaskFan2_t, 0);
-        xTaskCreatePinnedToCore(TaskPel, "TaskPel", 2048, NULL, 1, &TaskPel_t, 0);
-        xTaskCreatePinnedToCore(TaskPel2, "TaskPel2", 2048, NULL, 1, &TaskPel2_t, 0);
+        xTaskCreatePinnedToCore(TaskFan1, "TaskFan", 2048, NULL, 1, &TaskFan1_t, 0);
+        xTaskCreatePinnedToCore(TaskFan2, "TaskFan2", 2048, NULL, 1, &TaskFan2_t, 0);
+        xTaskCreatePinnedToCore(TaskTempCheck, "TaskTempCheck", 2048, NULL, 1, &TaskTempCheck_t, 0);
+        xTaskCreatePinnedToCore(TaskPelControl, "TaskPelControl", 2048, NULL, 1, &TaskPelControl_t, 0);
         xTaskCreatePinnedToCore(TaskPump, "TaskPump", 1024, NULL, 1, &TaskPump_t, 0);
         TimerCooldown_t = xTimerCreate("TimerCooldown", pdMS_TO_TICKS(20000), pdTRUE, (void*)0, reinterpret_cast<TimerCallbackFunction_t>(cooldownTimer));
         TimerCooldown2_t = xTimerCreate("TimerCooldown2", pdMS_TO_TICKS(20000), pdTRUE, (void*)0, reinterpret_cast<TimerCallbackFunction_t>(cooldownTimer2));
     }
-
-    // init();
-
-
     pinMode(17, OUTPUT);
     pinMode(12, OUTPUT);
-
-//     lv_init();
-
-// #if LV_USE_LOG != 0
-//     lv_log_register_print_cb( my_print ); /* register print function for debugging */
-// #endif
-
-//     tft.begin();          /* TFT init */
-//     tft.setRotation( 1 ); /* Landscape orientation, flipped */
-
-//     lv_disp_draw_buf_init( &draw_buf, buf, NULL, screenWidth * screenHeight / 10 );
-
-//     /*Initialize the display*/
-//     static lv_disp_drv_t disp_drv;
-//     lv_disp_drv_init( &disp_drv );
-//     /*Change the following line to your display resolution*/
-//     disp_drv.hor_res = screenWidth;
-//     disp_drv.ver_res = screenHeight;
-//     disp_drv.flush_cb = my_disp_flush;
-//     disp_drv.draw_buf = &draw_buf;
-//     lv_disp_drv_register( &disp_drv );
-
-//     /*Initialize the (dummy) input device driver*/
-//     static lv_indev_drv_t indev_drv;
-//     lv_indev_drv_init( &indev_drv );
-//     indev_drv.type = LV_INDEV_TYPE_POINTER;
-//     indev_drv.read_cb = my_touchpad_read;
-//     lv_indev_drv_register( &indev_drv );
-
-
-
-//     ui_init();
-//     lv_obj_add_event_cb(ui_power, event_cb, LV_EVENT_VALUE_CHANGED, NULL);
 
     Serial.println( "Setup done" );
 }
